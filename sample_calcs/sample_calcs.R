150d99ea4908b3ea111f0a448dfc74f10c73e279
Max Barnes <barnesmaxmax@gmail.com>
2020-08-31 13:36:39 -0600

    Add sample calc Rmd

---
+++ sample_calcs/sample_calcs.R
@@ -0,0 +1,20 @@
+#Sample Calculations of first cell using Roanoke data3
+library(rmarkdown)
+
+#Sample Calculations
+---
+title: "Sample Calculations"
+output: 
+  html_document:
+    tox: true
+    theme: united
+---
+
+```{r echo = FALSE}
+$AM_HWY_TIME = 0.77$```
+
+
+$AM_HWY_TIME = .77$
+  
+  
+  
