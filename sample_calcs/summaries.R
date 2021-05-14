863c7a316f4c4535ddb163df2bfe5e6dcf9171c4
Max <barnesmaxmax@gmail.com>
2021-05-14 13:34:52 -0600

    Update gitignore and R summaries script

--- sample_calcs/summaries.R
+++ sample_calcs/summaries.R
@@ -5,6 +5,9 @@ library(rhdf5)
 library(rhdf5filters)
 library(Rhdf5lib)
 
+#move files to correct folder for each scenario
+file.copy(from="D:/road49/Output/01_ROWSUMS.DBF", to= "C:/projects/ustm_resiliency/sample_calcs/OUTPUTS/ROAD49ROWSUM.DBF")
+
 #link folder for files and add constants
 scenarios_folder <- "C:/projects/ustm_resiliency/sample_calcs/OUTPUTS/"
 mc_cost_coef <- -0.0016
@@ -63,17 +66,30 @@ total_costs <- summary_table %>%
   summarise(total_delta = sum(delta)) %>%
   mutate(
     value = total_delta / mc_cost_coef
-  )
+  ) %>% 
+  arrange(value)
+  
+total_costs1 <-total_costs[-1,]
+
 
 tibble(
-  total_costs)%>%
-  knitr::kable(caption = "Cost associated with closure per day", format = "markdown")
+  total_costs1)%>%
+  knitr::kable(caption = "Cost associated with closure per day", format = "markdown", digits = 2) 
 
 #compute logsum by location
 logsum_scenario50 <- read.dbf('C:/projects/ustm_resiliency/Base/road50/Output/01_ROWSUMS.DBF')
 logsum_internal <- logsum_scenario50[-c(1:30),] %>%
   rename(TAZID = TAZ)
 
+#figuring out why HBO is positive
+logsums %>% filter(purpose == "NHB") %>%
+  filter(scenario %in% c("BASE", "ROAD44"))%>%
+  pivot_wider(values_from = logsum, names_from = scenario)%>%
+  mutate(diff = ROAD44 - BASE, totdiff = sum(diff)) %>%
+  arrange(-diff)
+
+
+write_rds(logsums, 'C:/projects/ustm_resiliency/sample_calcs/logsum_table.rds')
 #logsum origin destination county calculations, need to adjust tables first so that I can join
 #them together and end up with a table that has all the desired information in it
 
@@ -110,3 +126,49 @@ logsum_summary <- logsum_costs %>%
   mutate(
     cost_value = total_delta / mc_cost_coef)
 logsum_summary
+
+
+
+#change whole rowsums table to display info by county for each scenario
+#TAZID, purpose, value
+
+logsum_internal_all <- logsums%>%
+  rename(TAZID = TAZ)
+
+table3_all <- logsum_internal_all %>% 
+  pivot_longer(
+    cols = starts_with("LN"),
+    names_to = "purpose"
+  ) %>%
+  mutate(
+    purpose = gsub("LN", "", purpose))
+
+# add CO_NAME and productions
+table1_all <- left_join(taz, prod, by = c("TAZID" = "TAZ")) %>%
+  select(TAZID, CO_NAME, purpose, productions) %>%
+  left_join(logsums, by = c("TAZID" = "TAZ", "purpose"))
+
+#compute total, delta, pct_delta
+logsum_costs_all <- table1_all %>%
+  group_by(CO_NAME, purpose)%>%
+  pivot_wider(names_from = scenario, values_from = logsum)%>%
+  pivot_longer(cols = matches("ROAD"), names_to = "scenario", names_prefix = "ROAD", values_to = "logsum")%>%
+  mutate(
+    total = productions * logsum,
+    basetotal = productions * BASE,
+    delta = total - basetotal,
+    pct_delta = delta / basetotal * 100)%>%
+  filter(basetotal > 0)
+
+# get total delta and cost associated with closure by county
+#logsum_summary_all <- 
+logsum_costs_all %>%
+  group_by(CO_NAME, purpose, scenario) %>%
+  summarise(total_delta = sum(delta)) %>%
+  mutate(
+    cost_value = total_delta / mc_cost_coef * 1 / 100
+  ) %>% 
+  select(-total_delta) %>% 
+  pivot_wider(names_from = scenario, names_prefix = "Road ", values_from = cost_value)
+logsum_summary_all
+
